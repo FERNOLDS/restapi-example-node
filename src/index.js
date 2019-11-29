5f045decb9dab664b604660a55efcacbc0e32bfc
FERNOLDS ALVARADO <49449933+FERNOLDS@users.noreply.github.com>
2019-11-29 19:20:43 -0300

    primeros cambios

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+//settings
+app.set("port", process.env.PORT || 3000);
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+    res.send("Como estas papaso");
+
+});
+
+
+//starting the server
+app.listen(3000, () => {
+    console.log("Server on port ${app.get("port")}");
+});
